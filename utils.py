f40434c762339d3027e1cb97204dcc14d47917b9
Julien Bernardo <57379630+Julien-Bernardo@users.noreply.github.com>
2021-03-10 20:02:33 +0100

    Updated with comments and notebook

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def data_preprocessing(self, yelp_file_txt):
+
+def data_preprocessing(yelp_file_txt, data_path):
+    '''
+    Function outputs a clean dataframe with columns user, item, rating
+    from the yelp dataset
+    '''
+
+    # Loading yelp datset and keep only required columns
     raw_data = pd.read_table(yelp_file_txt)
     raw_data = raw_data.drop(raw_data.columns[-1], axis=1)
     raw_data.columns = ['user', 'item', 'rating']
@@ -14,28 +21,46 @@ def data_preprocessing(self, yelp_file_txt):
     data['user_id'] = data['user'].astype("category").cat.codes
     data['item_id'] = data['item'].astype("category").cat.codes
 
-    # Create a lookup frame so we can get the artist names back in
-    # readable form later.
-    item_lookup = data[['item_id', 'item']].drop_duplicates()
-    item_lookup['item_id'] = item_lookup.item_id.astype(str)
-
+    # Keeping only user_id and item_id
     data = data.drop(['user', 'item'], axis=1)
 
-    data.to_csv(self.data_path, sep=",", header=True, index=False)
+    data.to_csv(data_path, sep=",", header=True, index=False)
     return data
 
 class tools():
 
     def __init__(self, rating_list, data_path):
-        self.data_path = data_path
         self.nb_items = len(pd.read_csv(data_path)['item_id'].unique())  # nb of items
         self.nb_users = len(pd.read_csv(data_path)['user_id'].unique())  # nb of users
         self.rating_list = rating_list
 
     def set_data_matrix(self):
-
-        M = np.full((self.nb_users, self.nb_items), 0, dtype=np.float64)  # creating 0 matrix user * items
+        '''
+        Function returns matrix MxN (users vs items) with true ratings
+        '''
+        R = np.full((self.nb_users, self.nb_items), 0, dtype=np.float64)  # creating 0 matrix user * items
         for data in self.rating_list:
-            M[data[1], data[2]] = data[0]
-        return M
+            R[data[1], data[2]] = data[0]
+        return R
+
+    def set_R_hat(self, U, V):
+        '''
+        Function returns matrix MxN (users vs items) with predicted ratings
+        '''
+        R_hat_empty = np.full((self.nb_users, self.nb_items), 0, dtype=np.float64)  # creating 0 matrix user * items
+        pred_matrix = U @ V.T
+        for data in self.rating_list:
+            R_hat_empty[round(data[1]), round(data[2])] = pred_matrix[data[1], data[2]]
+        return R_hat_empty
+
+    def rmse(self, U, V):
+        '''
+        Compute RMSE between predicted ratings R_hat and true ratings R
+        '''
+        R_hat = self.set_R_hat(U, V)
+        R = self.set_data_matrix()
+        diff = R - R_hat
+        return np.sqrt(np.sum(np.power(diff, 2)) / (self.nb_users * self.nb_items))
+
+
 
